62d235d15f396765377cf95841e41beb83f22f72
cod-git <yuuto.1212.20@gmail.com>
2026-04-04 15:29:26 +0900

    Fixed client intents

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
   ]
 });
 
